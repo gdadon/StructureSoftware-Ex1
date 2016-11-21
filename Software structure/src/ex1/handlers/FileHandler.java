fcbe0c4015188555e79fe4b14facf28c1e2151f9
Tomer <Tomer@DESKTOP-N87NDG3>
2016-11-21 16:23:56 +0200

    Implements handlers

--- Software structure/src/ex1/handlers/FileHandler.java
+++ Software structure/src/ex1/handlers/FileHandler.java
@@ -1,36 +1,112 @@
 package ex1.handlers;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import ex1.resources.Graph;
 
 public class FileHandler {
-	
+
+	private static BufferedReader br;
+	private static String fileName = "";
+
 	/**
 	 * Read graph from file.
 	 * @param path - path of the graph file.
 	 * @return graph build from file description.
 	 */
 	public static Graph readGraph(String path) {
-		throw new UnsupportedOperationException();
-    }
-	
+		// create new empty graph
+		Graph g = new Graph();
+		try {
+			File fopen = new File(path);
+			br = new BufferedReader(new FileReader(fopen));
+			String currentLine = "";
+			// two first lines are number of nodes and edges
+			int nodes = Integer.parseInt(br.readLine());
+			int edges = Integer.parseInt(br.readLine());
+			g.setNumberOfNodes(nodes);
+			g.setNumberOfEdeges(edges);
+			// added each new vertex
+			for (int i = 0; i < nodes; i++) {
+				g.addVertex(i);
+			}
+			// added each new edge
+			while ((currentLine = br.readLine()) != null) {
+				String[] splitLine = currentLine.split(" ");
+				g.addEdge(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]), Double.parseDouble(splitLine[2]));
+			}
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return g;
+	}
+
 	/**
 	 * Handler to read the queries from give file
 	 * @param path - file to read queries from.
 	 * @return ArrayList of String[] with the queries.
 	 */
 	public static ArrayList<String[]> readQueries(String path){
-		throw new UnsupportedOperationException();
-	}
-	
-	/**
-	 * create answers file with corresponding answers to the queries file. 
-	 * @param answers - the answers to be written into file
-	 */
-	public static void writeAnswers(HashMap<String[], String> answers){
-		throw new UnsupportedOperationException();
+
+		ArrayList<String[]> queries = new ArrayList<String[]>();
+		try {
+			File fopen = new File(path);
+			fileName = fopen.getName();
+			fileName = fileName.substring(0, fileName.indexOf('.'));
+			br = new BufferedReader(new FileReader(fopen));
+			// read each query and split it
+			String currentLine = br.readLine();
+			while ((currentLine = br.readLine()) != null) {
+				String[] splitLine = currentLine.split(" ");
+				queries.add(splitLine);
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return queries;
 	}
 
+	/**
+	 * create answers file with corresponding answers to the queries file. 
+	 * @param respond - the answers to be written into file
+	 */
+	public static void writeAnswers(Map<String[], String> respond){
+		fileName += ".txt";
+		File output = new File("Output/"+fileName);
+		try {
+//			if(!output.createNewFile()){
+//				System.out.println("Failed to create answers file.");
+//			}
+			FileWriter fw = new FileWriter(output);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			Set<String[]> keys = respond.keySet();
+			for (Iterator<String[]> i = keys.iterator(); i.hasNext();) {
+				String[] key = i.next();
+				String answer = "";
+				for (String string : key) {
+					answer += string+" ";
+				}
+				String value = respond.get(key);
+				bw.write(answer + value + "\n");
+//				System.out.println(answer + value);
+			}
+			bw.flush();
+			bw.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Finish with queries to : "+ output.getPath());
+
+	}
 }
--- Software structure/src/ex1/handlers/QueryHandler.java
+++ Software structure/src/ex1/handlers/QueryHandler.java
@@ -1,19 +1,58 @@
 package ex1.handlers;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
+import ex1.algo.Graph_algo;
 import ex1.resources.Graph;
 
 public class QueryHandler {
-	
+
 	/**
 	 * Handler to queries, get array of queries and returns its answeres.
 	 * @param g - graph to run queries on.
 	 * @param queries - queries to run.
 	 * @return - HashMap of the form <Query, Answer>
 	 */
-	public static HashMap<String[],String> handlQueries(Graph g, ArrayList<String[]> queries){
-		throw new UnsupportedOperationException();
+	public static Map<String[],String> handlQueries(Graph g, ArrayList<String[]> queries){
+		Map<String[], String> respond = new LinkedHashMap<String[], String>();
+
+		for (String[] query : queries) {
+			int vStart, vEnd, blSize;
+			double dist;
+			// clear the graph to its begin mode
+			Graph.clearGraph(g);
+			switch(query.length){
+			case 1:
+				// info query
+				respond.put(query, Graph_algo.getGraphStatics(g));
+				break;
+			case 3:
+				// shortest path between - without Black List
+				vStart = Integer.parseInt(query[0]);
+				vEnd = Integer.parseInt(query[1]);
+				dist = Graph_algo.getShortestPathBetween(g, vStart, vEnd);
+				respond.put(query, ""+dist);
+				break;
+			default:
+				// shortest path between - with Black List
+				vStart = Integer.parseInt(query[0]);
+				vEnd = Integer.parseInt(query[1]);
+				blSize = Integer.parseInt(query[2]);
+				LinkedList<Integer> blackList = new LinkedList<Integer>();
+				// i = 3 - 3 is where the blackList vertice begin
+				for (int i = 1; i < blSize+1; i++) {
+					int v = Integer.parseInt(query[2+i]);
+					blackList.add(v);
+				}
+				dist = Graph_algo.getShortestPathWithBlackList(g, vStart, vEnd, blackList);
+				respond.put(query, ""+dist);
+				break;
+			}
+		}
+		return respond;
+
 	}
 }
