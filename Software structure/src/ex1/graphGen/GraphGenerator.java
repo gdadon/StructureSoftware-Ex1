6ea4e3823978efe4e8f8286b187ce45d307fc91b
michael <michael@michaelior>
2016-11-25 11:59:29 +0200

    fix

--- Software structure/src/ex1/graphGen/GraphGenerator.java
+++ Software structure/src/ex1/graphGen/GraphGenerator.java
@@ -8,7 +8,7 @@ import ex1.resources.Graph;
 
 public class GraphGenerator {
 
-	Graph g;
+	private Graph g;
 	static int numOfGraphText;
 	
 	/**
@@ -19,10 +19,15 @@ public class GraphGenerator {
 		numOfGraphText = 1;
 	}
 	
+	/**
+	 * constructor
+	 * @param RetryNum - num of this retry
+	 */
 	public GraphGenerator(int RetryNum) {
 		g = new Graph();
 		numOfGraphText = RetryNum;
 	}
+	
 	/**
 	 * Generate a new graph and print him in text file
 	 * @param vertex - amount of vertex in the graph
@@ -36,28 +41,30 @@ public class GraphGenerator {
 
 	private Graph GraphGenerat(int sumVertex) {
 
-		if (sumVertex > 0) {
+		if (sumVertex > 0)
+		{
 			int id = 0, temp = 0, randomIfAddEdge;
 			boolean oneEdgeAtLeast = false;
 			g.addVertex(id);
 			sumVertex--;
 			id++;
-			while (sumVertex > 0) {
+			while (sumVertex > 0) 
+			{
 				g.addVertex(id);
 				oneEdgeAtLeast = false;
-				while (!oneEdgeAtLeast) {
+				while (!oneEdgeAtLeast)
+				{
 					temp = id;
-					while (temp > 0) {
+					while (temp > 0) 
+					{
 						randomIfAddEdge = (int) (Math.random() * 2);
-						if (randomIfAddEdge == 1) {
+						if (randomIfAddEdge == 1)
+						{
 							double randomWeight = (double) (Math.random() * 100) + 1;
 							g.addEdge(id, --temp, randomWeight);
 							oneEdgeAtLeast = true;
-						} else
-							temp--;
-					}
-				}
-
+						} else temp--;
+					}}
 				id++;
 				sumVertex--;
 			}
@@ -72,16 +79,18 @@ public class GraphGenerator {
 		vec.add(-1);
 		int amountEdge = 0;
 
-		for (int i = 0; i < vertex; i++) {
-			for (Edge e : g.getVertexById(i).getAdjacencies()) {
-				if (e.getTarget().getId() > i) {
+		for (int i = 0; i < vertex; i++)
+		{
+			for (Edge e : g.getVertexById(i).getAdjacencies()) 
+			{
+				if (e.getTarget().getId() > i)
+				{
 					vec.add(i);
 					vec.add(e.getTarget().getId());
 					vec.add(e.getWeight());
 					amountEdge++;
 				}
 			}
-
 		}
 		vec.setElementAt(amountEdge, 1);
 		return vec;
@@ -99,10 +108,14 @@ public class GraphGenerator {
 			}	
 			writer.close();
 		} catch (Exception e) {
-			// do something
+				e.printStackTrace();
 		}
 	}
 
+	/***
+	 * main for example and try
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -110,13 +123,5 @@ public class GraphGenerator {
 			GraphGenerator g = new GraphGenerator(i+1);
 			g.GenerateAndPrint((int)(Math.random()*20)+1);
 		}
-		
-		
-		
-			
-		
-		
-
 	}
-
 }
